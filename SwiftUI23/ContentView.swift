d13ac9e861a5384a010d64c5368cf153d9367783
Rohit Saini <sketchlearn7@gmail.com>
2020-08-02 22:27:00 +0530

    code updated

--- SwiftUI23/ContentView.swift
+++ SwiftUI23/ContentView.swift
@@ -9,13 +9,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentPage = 0
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            ZStack{
+                if currentPage == 0{
+                     Image("page1").resizable().frame(height: 400).cornerRadius(20).padding()
+                }
+                else if currentPage == 1{
+                     Image("page2").resizable().frame(height: 400).cornerRadius(20).padding()
+                }
+                else{
+                   Image("page3").resizable().frame(height: 400).cornerRadius(20).padding()
+                }
+            }
+            PageControlView(current: currentPage)
+            HStack{
+                Button(action: {
+                    if self.currentPage != 0 {
+                        self.currentPage = self.currentPage - 1
+                    }
+                }) {
+                    Text("Previous").fontWeight(.heavy).padding()
+                }
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                Button(action: {
+                    if self.currentPage < 3{
+                        self.currentPage = self.currentPage + 1
+                    }
+                }) {
+                    Text("Next").fontWeight(.heavy).padding()
+                }
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
+        }.animation(.spring())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct PageControlView:UIViewRepresentable{
+    var current = 0
+    func makeUIView(context: UIViewRepresentableContext<PageControlView>) -> UIPageControl {
+        let page = UIPageControl()
+        page.currentPageIndicatorTintColor = .black
+        page.numberOfPages = 3
+        page.pageIndicatorTintColor = .gray
+        return page
+    }
+    func updateUIView(_ uiView: UIPageControl, context: UIViewRepresentableContext<PageControlView>) {
+        uiView.currentPage = current
     }
 }
